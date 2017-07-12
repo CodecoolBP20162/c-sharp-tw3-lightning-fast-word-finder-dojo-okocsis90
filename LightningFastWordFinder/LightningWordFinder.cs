4ed7af8dd23910b653b2970b4aa6d37cc27b5db2
okocsis90 <o.kocsis90@gmail.com>
2017-07-12 10:28:54 +0200

    first working solution

--- LightningFastWordFinder/LightningWordFinder.cs
+++ LightningFastWordFinder/LightningWordFinder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace LightningFastWordFinder
 {
@@ -6,7 +7,17 @@ namespace LightningFastWordFinder
     {
         public string GetLongestWord(string text)
         {
-            return "longestWord";
+            string longest = "";
+            string[] splitted = text.Split(' ');
+            foreach (string word in splitted)
+            {
+                string currentWord = Regex.Match(word, "[a-zA-Z]+").Value;
+                if (currentWord.Length > longest.Length)
+                {
+                    longest = currentWord;
+                }
+            }
+            return longest;
         }
     }
 }
